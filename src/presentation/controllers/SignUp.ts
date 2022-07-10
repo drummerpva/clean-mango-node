6e9924401ca7d7755009293f9966e1fb1c752206
Douglas Poma <douglaspoma@yahoo.com>
2022-07-10 15:19:37 -0300

    refactor: (SignUpController): create helper to return bad request

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,19 +1,14 @@
 import MissingParamError from "../errors/MissingParamError";
+import { badRequest } from "../helpers/http-helper";
 import { HttpRequest } from "../protocols/http";
 
 export default class SignUpController {
   async handle(httpRequest: HttpRequest): Promise<any> {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError("name"),
-      };
+      return badRequest(new MissingParamError("name"));
     }
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError("email"),
-      };
+      return badRequest(new MissingParamError("email"));
     }
   }
 }
