f1bffb101ee71d69268e9bd18b724785d049c460
Douglas Poma <douglaspoma@yahoo.com>
2022-07-10 22:01:21 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globalSetup: "./node_modules/@shelf/jest-mongodb/setup.js",
+  globalTeardown: "./node_modules/@shelf/jest-mongodb/teardown.js",
+  roots: ["<rootDir>/src", "<rootDir>/test"],
 };
