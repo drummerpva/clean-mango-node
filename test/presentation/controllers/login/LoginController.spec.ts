3b95eb3fa2b16cf08c3e20ee9e3c4d25545cbe61
Douglas Poma <douglaspoma@yahoo.com>
2022-07-12 20:38:42 -0300

    refactor: change mock to return value instead of implementation

--- test/presentation/controllers/login/LoginController.spec.ts
+++ test/presentation/controllers/login/LoginController.spec.ts
@@ -106,9 +106,9 @@ describe("LoginController", () => {
   });
   test("Should return 500 if Authentication throws", async () => {
     const { sut, authenticationStub } = makeSut();
-    jest.spyOn(authenticationStub, "auth").mockImplementationOnce(async () => {
-      throw new Error();
-    });
+    jest
+      .spyOn(authenticationStub, "auth")
+      .mockReturnValueOnce(Promise.reject(new Error()));
     const response = await sut.handle(makeFakeRequest());
     expect(response).toEqual(serverError(new Error()));
   });
