d36b7fc2422a524914775c1240ee4b92969ecc05
Douglas Poma <douglaspoma@yahoo.com>
2022-07-12 22:56:50 -0300

    test(ValidationComposite): ensure return error who validate return

---
+++ test/presentation/controllers/helpers/validators/ValidationComposite.spec.ts
@@ -0,0 +1,24 @@
+import { MissingParamError } from "../../../../../src/presentation/errors";
+import { Validation } from "../../../../../src/presentation/helpers/validator/Validation";
+import { ValidationComposite } from "../../../../../src/presentation/helpers/validator/ValidationComposite";
+
+const makeValidationStub = () => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error | null {
+      return new MissingParamError("field");
+    }
+  }
+  return new ValidationStub();
+};
+const makeSut = () => {
+  const validationStub = makeValidationStub();
+  const sut = new ValidationComposite([validationStub]);
+  return { sut, validationStub };
+};
+describe("ValidationComposite", () => {
+  test("Should return an error if any validation fails", () => {
+    const { sut } = makeSut();
+    const error = sut.validate({ field: "any_value" });
+    expect(error).toEqual(new MissingParamError("field"));
+  });
+});
